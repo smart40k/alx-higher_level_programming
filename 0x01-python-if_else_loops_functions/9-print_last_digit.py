f1eb2622d6367a72a745c2ee06f9321f42a30e9e
smart40k <smartdaction@gmail.com>
2022-11-29 15:49:09 -0800

    love

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def uppercase(str):
-    new_str = ""
-    for i in range(len(str)):
-        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
-            new_str += chr(ord(str[i]) - 32)
-            continue
-        new_str += str[i]
+def print_last_digit(number):
+    if number >= 0:
+        last_digit = number % 10
+    elif number < 0:
+        last_digit = (number * -1) % 10
 
-    print('{0}'.format(new_str))
+    print(last_digit, end='')
+    return last_digit
